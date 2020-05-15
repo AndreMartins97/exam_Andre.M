dc0b2966e02f64ec19e16839a84615f529a7ac82
AndreMartins97 <62611693+AndreMartins97@users.noreply.github.com>
2020-05-15 17:04:22 +0200

    Add files via upload
    sql

---
+++ recette.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 15-Maio-2020 às 14:35
+-- Versão do servidor: 10.4.10-MariaDB
+-- versão do PHP: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `ravioles`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `recette`
+--
+
+DROP TABLE IF EXISTS `recette`;
+CREATE TABLE IF NOT EXISTS `recette` (
+  `title` varchar(100) NOT NULL,
+  `description` varchar(250) NOT NULL,
+  `date` datetime NOT NULL,
+  `image` varchar(100) NOT NULL,
+  PRIMARY KEY (`title`),
+  UNIQUE KEY `title_UNIQUE` (`title`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
